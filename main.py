312e5d62fea4526b79df29cc918174b09f46ea47
Harsha <harshahm0130@gmail.com>
2024-06-22 23:39:50 +0530

    Sunrise and sunset

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# import requests
-#
+import requests
+from datetime import datetime
+
+MY_LAT = 12.971599
+MY_LONG = 77.594566
+
 # response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # response.raise_for_status()
 #
@@ -8,3 +12,22 @@
 #
 # iss_position = (longitude, latitude)
 # print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
+
